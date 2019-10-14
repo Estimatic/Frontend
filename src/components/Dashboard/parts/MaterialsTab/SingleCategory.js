706436413006e8289508e532cb06f8d9d75cef7b
Thomas Hessburg <44884308+TomHessburg@users.noreply.github.com>
2019-10-14 00:45:40 -0400

    created cat btn and route

--- src/components/Dashboard/parts/MaterialsTab/SingleCategory.js
+++ src/components/Dashboard/parts/MaterialsTab/SingleCategory.js
@@ -2,9 +2,11 @@ import React, { useState } from "react";
 import styled from "styled-components";
 import SingleMaterial from "./SingleMaterial";
 import { connect } from "react-redux";
+import { withRouter } from "react-router-dom";
 
 import ArrowRight from "@material-ui/icons/ArrowRight";
 import ArrowDropDown from "@material-ui/icons/ArrowDropDown";
+import Button from "@material-ui/core/Button";
 
 function SingleCategory(props) {
   const { secondary_color } = props.ui.colors;
@@ -14,16 +16,31 @@ function SingleCategory(props) {
   };
   return (
     <Wrapper secondary_color={secondary_color}>
-      <ArrowWrapper>
-        {open ? (
-          <StyledArrowDown onClick={handleOpen} />
-        ) : (
-          <StyledArrowRight onClick={handleOpen} />
-        )}
-      </ArrowWrapper>
-      <h4>
-        {props.category.name} <span>- {props.category.unit}</span>{" "}
-      </h4>
+      <TopWrapper>
+        <ArrowWrapper>
+          {open ? (
+            <StyledArrowDown onClick={handleOpen} />
+          ) : (
+            <StyledArrowRight onClick={handleOpen} />
+          )}
+        </ArrowWrapper>
+        <h4>
+          {props.category.name} <span>- {props.category.unit}</span>{" "}
+        </h4>
+        <Button
+          style={{
+            color: secondary_color || "#3B49B8"
+          }}
+          size="small"
+          onClick={() => {
+            props.history.push(
+              `/app/materials/add_material/${props.category._id}`
+            );
+          }}
+        >
+          Add Material
+        </Button>
+      </TopWrapper>
       <div style={!open ? { display: "none" } : null}>
         {props.category.materials.map(mat => {
           return <SingleMaterial material={mat} key={mat._id} />;
@@ -42,7 +59,7 @@ const mapStateToProps = state => {
 export default connect(
   mapStateToProps,
   {}
-)(SingleCategory);
+)(withRouter(SingleCategory));
 
 const Wrapper = styled.div`
   background: #fafafa;
@@ -51,13 +68,11 @@ const Wrapper = styled.div`
   padding: 16px 0;
   margin-bottom: 16px;
   box-shadow: 0 0 2px rgba(0, 0, 0, 0.2);
-  position: relative;
 
   h4 {
     font-style: normal;
     font-size: 24px;
-    margin-bottom: 8px;
-    margin-top: 0;
+    margin: 0;
     text-align: center;
     color: ${props => props.secondary_color || "#7A7A7A"};
     span {
@@ -71,18 +86,20 @@ const Wrapper = styled.div`
   }
 `;
 
-const ArrowWrapper = styled.div`
-  position: absolute;
-  top: 20px;
-  right: 8px;
-`;
+const ArrowWrapper = styled.div``;
 
 const StyledArrowRight = styled(ArrowRight)`
-  transform: scale(1.5);
+  transform: scale(1.2);
   cursor: pointer;
 `;
 
 const StyledArrowDown = styled(ArrowDropDown)`
-  transform: scale(1.5);
+  transform: scale(1.2);
   cursor: pointer;
+`;
+const TopWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 16px;
 `;
